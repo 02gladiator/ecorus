f19f1d49bd13de4f85b22fffe52f0520c9394f28
02gladiator <02gladiator@gmail.com>
2025-05-28 22:38:09 +0300

    homePage
    home page for ecorus

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import {Header} from "./components/Header";
 import {Footer} from "./components/Footer";
-import {Slider} from "./components/Slider";
-import {HomePromoSection} from "./components/PromoSection";
+import {Route, Routes} from "react-router-dom";
+import {HomePage} from "./pages/Home";
 
 
 function App() {
 
-  return (
-    <>
-      <Header token={"null"}></Header>
-        <Slider />
-      <HomePromoSection></HomePromoSection>
-        <Footer />
-    </>
-  )
+    return (
+        <>
+            <Header token={"null"}></Header>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+            </Routes>
+            <Footer/>
+        </>
+    )
 }
 
 export default App
